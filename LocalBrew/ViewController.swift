22ed469b7a237df9f4fda6bbae69756d06502c8b
richardgmartin <richardgeorgemartin@gmail.com>
2016-02-13 09:26:31 -0600

    initial master branch commit

---
+++ LocalBrew/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LocalBrew
+//
+//  Created by Richard Martin on 2016-02-13.
+//  Copyright © 2016 Richard Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+LocalBrew is an iOS app that provides the user with an easy to access
+directory of micro breweeries in different cities across the US and Canada.
